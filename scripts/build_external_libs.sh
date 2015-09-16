2c6e53d495121f33972c060465d15d3975e2ce94
Wojciech Migda <wojtek.golf@interia.pl>
2015-09-16 20:53:00 +0200

    building mpg123

---
+++ scripts/build_external_libs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+MY_DIR=`dirname ${0}`
+
+MPG123_DIR=$( cd ${MY_DIR}/../mpg123/ && pwd )
+INSTALL_DIR=$( cd ${MY_DIR}/../external/ && pwd )
+
+if [ "$(uname)" == "Darwin" ]; then
+    MAKE=make
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    MAKE=make
+    MPG123_CPU_ARG="--with-cpu=sse"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    MAKE=mingw32-make
+    BUILD_TYPE_ARG="--build=i686-pc-mingw32"
+    MPG123_CPU_ARG="--with-cpu=x86-64"
+fi
+
+############################################################
+### build mpg123
+############################################################
+
+pushd ${MPG123_DIR}
+
+libtoolize --force && \
+aclocal && \
+autoheader && \
+automake --force-missing --add-missing && \
+autoconf
+
+./configure ${BUILD_TYPE_ARG} --prefix=${INSTALL_DIR} --enable-static --with-optimization=3 ${MPG123_CPU_ARG} && ${MAKE} -j 3 && make install-exec install-data
+
+popd
