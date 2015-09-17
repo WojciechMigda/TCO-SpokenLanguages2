1871a98e3b53625e69a77259ec6c8a1b59c78018
Wojciech Migda <wojtek.golf@interia.pl>
2015-09-17 21:37:38 +0200

    reading CSV & MP3 done, evaluating MFCC

--- src/gen_mfcc_feat.jl
+++ src/gen_mfcc_feat.jl
@@ -1,4 +1,4 @@
-#!/usr/bin/julia
+#!/usr/bin/env julia
 ################################################################################
 #
 #  Copyright (c) 2015 Wojciech Migda
@@ -25,32 +25,91 @@
 #
 ################################################################################
 
-sig = Array(Int16, 18000)
-println(size(sig, 1))
+using MFCC
 
 type Mp3Params
-  rate::Uint32
-  channels::Uint32
-  encoding::Uint32
+    rate::Uint32
+    channels::Uint32
+    encoding::Uint32
 end
 
-mp3Params = Mp3Params(uint32(0), uint32(0), uint32(0))
 
-res = ccall((:mp3decoder, "./libmp3decoder"), Csize_t, (Ptr{Int8}, Ptr{Uint8}, Csize_t, Ptr{Mp3Params}), "../../data/0.mp3", sig, size(sig, 1) * 2, &mp3Params)
+function mp3decoder!(ifname::String, osig::AbstractVector{Int16})
+    # we expect 10 secs of samples, int16, stereo at 44100 Hz
+    #const NSAMP = 10 * 44100
+    #const NCHAN = 2
+    #sig = Array(Int16, NSAMP * NCHAN)
+    mp3params = Mp3Params(uint32(0), uint32(0), uint32(0))
+    const NREAD =
+#        ccall((:mp3decoder, "libmp3decoder"),
+        ccall((:mp3decoder, LIB_MP3_DECODER),
+            Csize_t, (Ptr{Uint8}, Ptr{Void}, Csize_t, Ptr{Mp3Params}),
+            ifname, osig, size(osig, 1) * sizeof(osig[1]), &mp3params)
+    @assert mp3params.encoding == 0xD0 # MPG123_ENC_SIGNED_16
 
+    return NREAD, mp3params
+end
 
-println(res)
-println("rate: ", mp3Params.rate, ", channels: ", mp3Params.channels, ", encoding: ", mp3Params.encoding)
-#println(sig)
+function process_range{_Tp}(inames::AbstractVector{_Tp}, istart::Int, iend::Int)
+    const NELEM = iend - istart
+    for idx in [istart:iend]
+        println("$(idx)   $(TRAIN_MP3)/$(TRAIN[idx, 1])")
+        const NSAMP = 10 * 44100
+        const NCHAN = 2
+        #sig = Array(Int16, NSAMP * NCHAN)
+        #const NREAD, MP3PARAMS = mp3decoder!("$(TRAIN_MP3)/$(TRAIN[idx, 1])", sig)
+    end
+end
 
-#train = readdlm("D:/Workspace/TCO-SpokenLanguages2/data/trainingData.csv", ',')
-train = readdlm("/repo/topcoder/SpokenLanguages2/data/trainingData.csv", ',')
-NROW = size(train, 1)
+function main()
+    const DATA_DIR = "$(THIS_DIR)/../../data"
+    const CSV_DIR = DATA_DIR
+    const TRAIN_CSV = "$(CSV_DIR)/trainingData.csv"
+    const TRAIN_MP3 = "$(DATA_DIR)/train"
 
-println(train[1, 1], "|", train[1, 2])
-println(train[2, 1], "|", train[2, 2])
-println(train[NROW, 1], "|", train[NROW, 2])
+    const TRAIN = readdlm(TRAIN_CSV, ',')
+    const NROW = size(TRAIN, 1)
 
-for idx in [2:NROW]
-    #println(x)
+    #println(typeof(TRAIN[1, :])) # Array{Any,2}
+    #println(typeof(TRAIN[:])) # Array{Any,1}
+    #println(typeof(TRAIN[:, 1])) # Array{Any,1}
+    #println(typeof(TRAIN[2, 1])) # SubString{ASCIIString}
+    #process_range(TRAIN[:, 1], 1, 1000)
+    println(TRAIN[1, 1], "|", TRAIN[1, 2])
+    println(TRAIN[2, 1], "|", TRAIN[2, 2])
+    println(TRAIN[NROW, 1], "|", TRAIN[NROW, 2])
+
+    #for idx in [2:NROW]
+    for idx in [2:2]
+        println("$(idx)   $(TRAIN_MP3)/$(TRAIN[idx, 1])")
+        const NSAMP = 10 * 44100
+        const NCHAN = 2
+        sig = Array(Int16, NSAMP * NCHAN)
+        const NREAD, MP3PARAMS = mp3decoder!("$(TRAIN_MP3)/$(TRAIN[idx, 1])", sig)
+
+        # MFCC
+        @time mfcc([float(x) for x in sig], MP3PARAMS.rate * 2.)
+        # elapsed time: 7.802742415 seconds (155064324 bytes allocated, 1.88% gc time)
+        @time foo = mfcc([float(x) for x in sig], MP3PARAMS.rate * 2.)
+        # elapsed time: 0.835244415 seconds (114095204 bytes allocated, 17.99% gc time)
+        println(typeof(foo))
+        println(size(foo[1]))
+        println(size(foo[2]))
+        println(foo[3])
+
+    end
+end
+
+if ~isinteractive()
+    const THIS_DIR = dirname(Base.source_path())
+    const LIB_MP3_DECODER = find_library(["libmp3decoder"], [THIS_DIR])
+
+    @windows? (
+    begin
+        ENV["PATH"] = "$(ENV["PATH"]);$(THIS_DIR);$(THIS_DIR)/../external/bin"
+        #println(ENV["PATH"])
+    end
+    : ()
+    )
+    main()
 end
