4f95624f1fdb5ffe0bfb707e6a7435cbb3e59049
Wojciech Migda <wojtek.golf@interia.pl>
2015-09-25 18:53:37 +0200

    improved encapsulation

--- src/gen_mfcc_feat.jl
+++ src/gen_mfcc_feat.jl
@@ -30,29 +30,37 @@ addprocs(div(CPU_CORES, 2))
 using HDF5
 @everywhere using Worker
 
-function main()
-    const DATA_DIR = "$(THIS_DIR)/../../data"
-    const TRAIN_MP3 = "$(DATA_DIR)/train"
+function generate(
+    MP3_DIR::ASCIIString,
+    MP3_H5_FNAME::ASCIIString,
+    MP3_H5_DSET::ASCIIString,
+    MFCC_H5_FNAME::ASCIIString,
+    MFCC_H5_DSET::ASCIIString)
+    
+    const TRAIN_MP3 = MP3_DIR
 
-    const TRAIN_X = h5read("trainXmp3.h5", "lid/train/X/mp3")
+    const TRAIN_X = h5read(MP3_H5_FNAME, MP3_H5_DSET)
 
-    #const TRAIN_SUBSETS = int(linspace(0, size(TRAIN, 1), (div(CPU_CORES, 2) + 1)))
-    const TRAIN_SUBSETS = int(linspace(0, 30, 5))
+    const TRAIN_SUBSETS = int(linspace(0, size(TRAIN_X, 1), (div(CPU_CORES, 2) + 1)))
+    #const TRAIN_SUBSETS = int(linspace(0, 30, 5))
     println(TRAIN_SUBSETS)
 
     foo = @parallel reduce for i = 1:length(TRAIN_SUBSETS) - 1
         Worker.gen_mfcc(
+            MP3_DIR,
             sub(TRAIN_X, TRAIN_SUBSETS[i] + 1:TRAIN_SUBSETS[i + 1]),
             TRAIN_SUBSETS[i] + 1)
     end
 
+    gc()
+    
     println(typeof(foo))
     const NROWS = TRAIN_SUBSETS[end]
     println(size(foo[][1][], 1))
     
-    fid = h5open("trainXmfcc.h5", "w")
+    fid = h5open(MFCC_H5_FNAME, "w")
     
-    Xdset = d_create(fid, "lid/train/X/mfcc", datatype(Float64), dataspace(size(foo[][1][], 1), NROWS))
+    Xdset = d_create(fid, MFCC_H5_DSET, datatype(Float64), dataspace(size(foo[][1][], 1), NROWS))
 
     for vt = 1:size(foo, 1)
         pos = foo[vt][2]
@@ -60,10 +68,22 @@ function main()
             Xdset[:, pos] = foo[vt][1][vx]
             pos += 1
         end
+        
+        gc()
     end
 
+    gc()
+    
     close(Xdset)
     close(fid)
+end
+
+function main()
+    const DATA_DIR = "$(THIS_DIR)/../../data"
+    
+    generate("$(DATA_DIR)/train", "trainXmp3.h5", "lid/train/X/mp3", "trainXmfcc.h5", "lid/train/X/mfcc")
+    generate("$(DATA_DIR)/test", "testXmp3.h5", "lid/test/X/mp3", "testXmfcc.h5", "lid/test/X/mfcc")
+
 end
 
 const THIS_DIR = dirname(Base.source_path())
